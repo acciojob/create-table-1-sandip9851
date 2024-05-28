714abd8a2d0c48a0b6c83cab2f37db4294bbf793
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-28 20:09:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    let row = document.createElement('tr'); // create row node
+    let col1 = document.createElement('td'); // create column node
+    let col2 = document.createElement('td'); // create second column node
+    col1.innerHTML = "New Cell1"; // append first column data
+    col2.innerHTML = "New Cell2"; // append second column data
+    row.appendChild(col1); // append first column to row
+    row.appendChild(col2); // append second column to row
+    table.prepend(row); // append row to table
 }
